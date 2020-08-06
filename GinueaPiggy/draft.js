ae35e928a29657f65210026a3be490e6b42ba3dc
Doktor-Doom <andre.o.martin@gmail.com>
2020-08-06 09:12:54 -0700

    Kondo some files

---
+++ GinueaPiggy/draft.js
@@ -0,0 +1,30 @@
+'use strict''
+
+// if(answer.toLowercase() === 'yes' || answer.toLowercase() === 'y'){
+//   return 'yes';
+// } else if (answer.toLowercase() === 'no' || answer.toLowercase() === 'n'){
+//   return 'n';
+// } else {
+//   return 'reprompt'
+// }
+
+
+// var 
+
+// while loop
+
+// var x = 0;
+
+// while(x <= 10){
+//   console.log('value of x ' + x);
+//   x = x + 1;
+// }
+
+// for loop
+
+// var manhattan = ['bourbon', 'bitters', 'vermouth' ];
+
+// for(var i=0; i < manhattan.length; i++){
+//   console.log('value i' + i);
+//   console.log('value array ' + manhattan[i]);
+// }
