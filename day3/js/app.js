0d46e1e47d743e9d00a88541847e2680fd5b78dd
Doktor-Doom <andre.o.martin@gmail.com>
2020-08-06 17:18:44 -0700

    scope add

--- day3/js/app.js
+++ day3/js/app.js
@@ -1,19 +1,22 @@
 'use strict'
 
-console.log(greetings)
+// console.log(greetings)
 
 var usernaamAntwoord;
 
-function wieBenJe(){
+var addScore = 0;            
+
+function whoDis(){
   usernaamAntwoord = prompt('How\'s it going? What\'s your name?'); 
   console.log('usernaamAntwoord :' + usernaamAntwoord);
 
   alert('Cool Beans, ' + usernaamAntwoord + ' hope you\'re having a good day');
+  addScore++;
 }
-wieBenJe();
+whoDis();
 
-
-function waarVanDan(){
+    
+function whereFrom(){
   var seattleOrigin = prompt('Am I from around here (Y/N)?');
   console.log('seattleOrigin:' + seattleOrigin);
 
@@ -21,16 +24,17 @@ function waarVanDan(){
     alert('Not Quite..');
   } else {
     alert('Appears Legitimate ' + usernaamAntwoord);
+    addScore++;
   } 
 }
-
-waarVanDan();
+whereFrom();
 
 function film(){
   var filmLover = prompt('Do I like movies (y/n)?');
   console.log('filmLover:' + filmLover);
   if(filmLover.toLowerCase() === 'y'){
     alert('Not too shabby.');
+    addScore++;
   } else {
     alert('Inconcievable!!');
   }
@@ -43,6 +47,7 @@ function kin(){
 
   if (famFam.toLowerCase() === 'y'){
     alert('Yep, Older sister.');
+    addScore++;
   }else if(famFam.toLowerCase() === 'n'){
     alert('she\'s small, but she still counts');
   }
@@ -54,6 +59,7 @@ function topShelf(){
   console.log('reach : ' + reach);
   if (reach.toLowerCase() === 'y'){
     alert('I do reach the top shelf, yes.');
+    addScore++;
   }else{
     alert('If I was shorter I wouldnt have a hard time finding shirts with the right length sleeves');
   }
@@ -75,6 +81,7 @@ function howManyPetsQuestion(){
 
     } else if(howManyPetsAnswer === '30'){
       alert('I know it seems a bit much...and it probably is BUT I don\'t care!');
+      addScore++;
       break;
     }
     // Thanks to Krystian for helping me understand this
@@ -83,21 +90,27 @@ function howManyPetsQuestion(){
 howManyPetsQuestion();
 
 function tooManyColors(){ 
-  
+  // var flag= false;
 for (var i=0; i<6; i++){
 
-    var favColorAnswer = prompt('What is Andre\'s favorite color?');
+    var favColorAnswer = prompt('I love 3 colors, guess one.');
 
     var favColorArray = ['purple', 'green', 'neon pink', 'orange', 'navy blue'];
 
     if(favColorAnswer === favColorArray[0] || favColorAnswer === favColorArray[1] ||favColorAnswer === favColorArray[2]){
       alert('You got it!');
-    } else {
+      addScore++;
+      // flag = true;
+      break;
+
+    } 
+    else{
       alert('A Swing and A Miss.');
     } 
     alert('possible answers included purple, green, neon pink, orange, navy blue');
 }
-
+}
+// Q6 and Q7 were merged here accidentally
 tooManyColors();
 
 function danceDance(){
@@ -105,12 +118,13 @@ function danceDance(){
   var doIDance = prompt('Doe\'s he dance (y/n)?');
     if (doIDance.toLowerCase() === 'y'){
     alert ('I only do that to make my wife laugh..');
+    addScore++;
   } else {
     alert('only to make the wife laugh');
+    addScore++;
   }
 }
 danceDance();
 
-prompt('Thank\'s for stopping by,' + usernaamAntwoord);
-
-} 
+alert('Thank\'s for stopping by,' + usernaamAntwoord + ' here\'s a bunch of points cuz you\'re a star. ' + addScore); 
+// Taylor helped me fix this and understand what went wrong originally. 
